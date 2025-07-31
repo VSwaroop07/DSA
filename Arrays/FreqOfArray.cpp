116e3f0a877e7da8952931110c7ec08d01768dbf
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2025-07-31 18:07:08 +0530

    Avg Of Array

---
+++ Arrays/FreqOfArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    double sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    int avg = sum/n;
+    cout<<sum;
+    cout<<avg;
+    return 0;
+
+}
