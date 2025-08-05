873542b923b47bc70be3e9b9f2fd8f99d959fdfe
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2025-08-05 22:03:24 +0530

    New Question

---
+++ Arrays/AscDscArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int arr[] = {4, 2, 5, 3, 6, 1, 8, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr + n);
+    int mid = 0;
+    if (n % 2 == 0)
+    {
+        int n1 = 0;
+    }
+    else
+    {
+
+        mid = n / 2;
+    }
+    for (int i = 0; i < mid; i++){}
+}
